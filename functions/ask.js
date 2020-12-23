b9d659a7298446b29aebea0fd343681598e10065
Frovu <sfrovis@gmail.com>
2020-12-24 01:10:28 +0300

    fix: die time

--- functions/ask.js
+++ functions/ask.js
@@ -2,7 +2,7 @@
 module.exports = function(msg, question) {
 	return new Promise((resolve) => {
 		msg.channel.send(question);
-		const collector = msg.channel.createMessageCollector(m => m.author.id === msg.author.id, {time: 30000});
+		const collector = msg.channel.createMessageCollector(m => m.author.id === msg.author.id, {time: 300000});
 		collector.on('collect', msg => {
 			if(['stop', 'q', 'abort'].includes(msg.content)) {
 				collector.stop('abort');
